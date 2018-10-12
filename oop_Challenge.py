b18848477bb375c6afc7754e850182b0aae1e31c
AndreaMendi <andreamendi@icloud.com>
2018-10-11 21:45:56 -0500

    OOP Challenge

---
+++ oop_Challenge.py
@@ -0,0 +1,24 @@
+class Account:
+    def __init__(self,owner,balance):
+        self.owner = owner
+        self.balance = balance
+
+        print("Owner:  {}".format(self.owner))
+        print("Balance:  {}".format(self.balance))
+    
+    def deposit(self, amount):
+        self.balance += amount 
+        return "Your actual balance is: {}".format(self.balance)
+        
+    def withdraw(self, withdraw):
+        if self.balance >= withdraw:
+            self.balance -= withdraw
+            return "Your actual balance is: {}".format(self.balance)
+        else:
+            return "Funds Unavailable!"
+
+acct1 = Account('Jose',100)
+print(acct1.deposit(400))
+print("--------------")
+print(acct1.withdraw(500))
+
